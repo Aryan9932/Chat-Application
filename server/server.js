5f61ef9c1cf120323b0e2d23cedc525ea1e3bb1e
Bhavesh-girase <bhaveshgirase27@gmail.com>
2025-07-20 14:59:38 +0530

    changes in server

--- server/server.js
+++ server/server.js
@@ -225,8 +225,8 @@ app.use("/api/messages", messageRouter);
 // Connect to MongoDB
 await connectDB();
 
-if(process.env.NODE_ENV !=="production"){const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log("🚀 Server is running on PORT:", PORT));
-}
+
 // for the vercel
 export default server;
